1fb0b5b4089dfa2d9bbf5ae3add7279a1f3ff2ba
guudc <65718626+guudc@users.noreply.github.com>
2024-03-23 09:47:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,17 @@
 "use strict"
 
-const fs = require('fs');
 const express = require("express")
-const https = require('https');
-const { createServer } = require('node:http');
 const route = require("./src/routes/routes.js") 
-const app = express() 
+const app = express()
 
 app.use("/", route)
-const server = createServer(app);
-
 
 let port = process.env.PORT || 4000
-server.listen(port, () => {
-    console.log('server running at port ' + port);
-}); 
- 
+
+app.listen(port, () => {
+    // eslint-disable-next-line no-console
+    console.log(`App listening on port ${port}`)
+})
+
 
  
